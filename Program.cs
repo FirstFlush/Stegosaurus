d665275a97e1f09091a3bd6216855a1e7784bc30
FirstFlush <firstflush@protonmail.com>
2025-03-09 15:54:48 -0700

    Configured logging with Serilog

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+using var serviceProvider = new ServiceCollection()
+    .AddLogging(builder => 
+    {
+        builder.ClearProviders();
+        builder.AddSerilog();
+    }).BuildServiceProvider();
